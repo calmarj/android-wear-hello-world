b2453c9f95d5de42e2fb785569cbd1235525808a
calmarj <calmarj@gmail.com>
2015-01-22 03:51:14 +0400

    Added Code for notification

--- mobile/src/main/java/me/smarthwatches/simplenotification/HandHeldActivity.java
+++ mobile/src/main/java/me/smarthwatches/simplenotification/HandHeldActivity.java
@@ -1,9 +1,14 @@
 package me.smarthwatches.simplenotification;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Notification;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class HandHeldActivity extends ActionBarActivity {
@@ -12,6 +17,26 @@ public class HandHeldActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hand_held);
+
+        Button mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Notification notification = new NotificationCompat.Builder(getApplication())
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Hello World")
+                        .setContentText("My first Android Wear notification")
+                        .extend(
+                                new NotificationCompat.WearableExtender().setHintShowBackgroundOnly(true))
+                        .build();
+
+                NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplication());
+
+                int notificationId = 1;
+                notificationManager.notify(notificationId, notification);
+            }
+        });
+
     }
 
 
